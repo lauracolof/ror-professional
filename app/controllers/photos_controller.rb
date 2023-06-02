5db4ff6e2c2eb3529f017d477dda283d1d1c1426
Laura <lauracolof@gmail.com>
2023-06-02 19:18:37 -0300

    create and edit register

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,11 +7,31 @@ class PhotosController < ApplicationController
   def new
   end
 
+  # /photos/:id/edit 
+  def edit
+    @id = params[:id]
+  end
+
+  def update
+    photo = Photo.find( params[:id] )
+    photo.title = params[:photo][:title]
+    photo.image_url = params[:photo][:image_url]
+
+    photo.save
+  end
+
+  def show 
+    @photo = Photo.find(params[:id])
+  end 
+
   def create
     photo = Photo.new
     photo.title = params[:photo][:title]
     photo.image_url = params[:photo][:image_url]
     photo.save
+
+    redirect_to '/photos/#{photo_id}'
+    # también podemos pasar directamente el obj "photo" 
   end
 
 end
